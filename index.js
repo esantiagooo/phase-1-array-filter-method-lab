2b478aa664ee3514e69e4cae38ebb2ef889c9616
Eric <eric.yanez50@qmail.cuny.edu>
2022-03-05 11:25:18 -0500

    commit tree 5f8efd43baa2953a9c9da68e7df36c7c9a318334

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+const findMatching = function(names, string){
+    return(names.filter(function(name){
+        return name.toLowerCase() === string.toLowerCase()
+    })
+    )
+}
